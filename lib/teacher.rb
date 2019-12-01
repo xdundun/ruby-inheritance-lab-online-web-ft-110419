c51b4e813a6057e88f443042bab08f72fe4a4b7c
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-01 21:20:54 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,7 +1,9 @@
 class Teacher < User
 
-  def teach
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-end
+
+  def teach
+    KNOWLEDGE.random
+  end
 
 end
